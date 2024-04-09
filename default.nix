bef22e6382e03eadf53e551422b5c2f1b2cd68e4
Nikolai Schimke <nikolais@tuta.io>
2024-04-09 18:32:53 +0100

    Update my-next-app version to 0.1.1 and fix next.config.mjs output

--- default.nix
+++ default.nix
@@ -1,9 +1,6 @@
+{ config, dream2nix, ... }:
 {
-  lib,
-  config,
-  dream2nix,
-  ...
-}: {
+
   imports = [
     dream2nix.modules.dream2nix.nodejs-package-lock-v3
     dream2nix.modules.dream2nix.nodejs-granular-v3
@@ -13,18 +10,16 @@
     src = ./my-next-app;
   };
 
-  deps = {nixpkgs, ...}: {
-    inherit
-      (nixpkgs)
-      fetchFromGitHub
-      stdenv
-      ;
-  };
+  deps =
+    { nixpkgs, ... }:
+    {
+      inherit (nixpkgs) stdenv nodejs;
+    };
 
   nodejs-package-lock-v3 = {
     packageLockFile = "${config.mkDerivation.src}/package-lock.json";
   };
 
   name = "my-next-app";
-  version = "0.1.0";
+  version = "0.1.1";
 }
--- flake.nix
+++ flake.nix
@@ -6,27 +6,63 @@
     nixpkgs.follows = "dream2nix/nixpkgs";
   };
 
-  outputs = inputs @ {
-    self,
-    dream2nix,
-    nixpkgs,
-    ...
-  }: let
-    system = "aarch64-darwin";
-  in {
-    # All packages defined in ./packages/<name> are automatically added to the flake outputs
-    # e.g., 'packages/hello/default.nix' becomes '.#packages.hello'
-    packages.${system}.default = dream2nix.lib.evalModules {
-      packageSets.nixpkgs = inputs.dream2nix.inputs.nixpkgs.legacyPackages.${system};
-      modules = [
-        ./default.nix
-        {
-          paths.projectRoot = ./.;
-          # can be changed to ".git" or "flake.nix" to get rid of .project-root
-          paths.projectRootFile = "flake.nix";
-          paths.package = ./.;
-        }
-      ];
+  outputs =
+    inputs@{
+      self,
+      dream2nix,
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+      package = dream2nix.lib.evalModules {
+        packageSets.nixpkgs = inputs.dream2nix.inputs.nixpkgs.legacyPackages.${system};
+        modules = [
+          ./default.nix
+          {
+            paths.projectRoot = ./.;
+            paths.projectRootFile = "flake.nix";
+            paths.package = ./.;
+          }
+        ];
+      };
+      shellScript = pkgs.writeShellApplication {
+        name = "start-next";
+
+        runtimeInputs = [ pkgs.nodejs ];
+
+        text = ''
+          cd ~/.nix-profile/lib/node_modules/my-next-app
+          ${pkgs.nodejs}/bin/npm run start
+        '';
+      };
+    in
+
+    {
+      packages.${system} = {
+        default = package;
+        start-next = shellScript;
+      };
+
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (
+            { pkgs, ... }:
+            {
+              systemd.services.run-next = {
+                description = "run my nextjs project";
+                after = [ "network.target" ];
+                wantedBy = [ "multi-user.target" ];
+                serviceConfig = {
+                  ExecStart = shellScript;
+                  Restart = "always";
+                };
+              };
+            }
+          )
+        ];
+      };
     };
-  };
 }
